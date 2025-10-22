8ea2a3eed20167142d36fba51b445831355fbeda
SHADOW-465 <kumaarsk465@gmail.com>
2025-10-22 21:22:16 +0530

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,11 +1,14 @@
-// convex/auth.config.js
-
-export default {
+const authConfig = {
   providers: [
     {
-      // Use the original variable name again
-      domain: process.env.CLERK_FRONTEND_API_URL, 
+      // Replace with your own Clerk Issuer URL from your "convex" JWT template
+      // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
+      // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
+      // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
+      domain: process.env.CLERK_FRONTEND_API_URL,
       applicationID: "convex",
     },
   ],
 };
+
+export default authConfig;
